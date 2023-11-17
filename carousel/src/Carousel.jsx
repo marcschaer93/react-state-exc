81751a363174f61c54c813b44409dd387823e53a
Marc Schär <marcschaer@Marcs-MacBook-Pro.local>
2023-11-17 18:23:02 +0100

    Part 3 done

--- carousel/src/Carousel.jsx
+++ carousel/src/Carousel.jsx
@@ -2,19 +2,18 @@ import { useState } from "react";
 import "./Carousel.css";
 import Card from "./Card";
 
-
 /** Carousel: displays images and arrows to navigate through them
- * 
+ *
  * Props:
  * - photos: array of {src, caption} objects
  * - title: string describing the collection of images
- * 
+ *
  * State:
  * - currCardIdx: integer for current card index
- * 
+ *
  * App --> Carousel --> Card
  */
- function Carousel({ photos, title }) {
+function Carousel({ photos, title }) {
   const [currCardIdx, setCurrCardIdx] = useState(0);
 
   const currCard = photos[currCardIdx];
@@ -22,27 +21,35 @@ import Card from "./Card";
 
   //Increments currCardIdx state by 1
   function goForward() {
-    setCurrCardIdx(currCardIdx + 1);
+    setCurrCardIdx((prevCardIdx) => (prevCardIdx + 1) % total);
   }
+
+  // const goBackward = () => {
+  //   if (currCardIdx === 0) {
+  //     setCurrCardIdx(total - 1);
+  //   } else {
+  //     setCurrCardIdx((prevCardIdx) => prevCardIdx - 1);
+  //   }
+  // };
+
+  // Same as goBackward above, but harder to understand with modulo
+  //
+  const goBackward = () => {
+    setCurrCardIdx((prevCardIdx) => (prevCardIdx - 1 + total) % total);
+  };
 
   return (
     <div className="Carousel">
       <h1>{title}</h1>
       <div className="Carousel-main">
-        <i
-          className="bi bi-arrow-left-circle"
-          onClick={goForward}
-        />
+        <i className="bi bi-arrow-left-circle" onClick={goBackward} />
         <Card
           caption={currCard.caption}
           src={currCard.src}
           currNum={currCardIdx + 1}
           totalNum={total}
         />
-        <i
-          className="bi bi-arrow-right-circle"
-          onClick={goForward}
-        />
+        <i className="bi bi-arrow-right-circle" onClick={goForward} />
       </div>
     </div>
   );
--- carousel/src/Carousel.test.jsx
+++ carousel/src/Carousel.test.jsx
@@ -42,3 +42,30 @@ it("matches snapshot", () => {
   );
   expect(asFragment()).toMatchSnapshot();
 });
+
+it("test left arrow function (move 1 picture back)", () => {
+  const { container } = render(
+    <Carousel photos={TEST_IMAGES} title="images for testing" />
+  );
+  // expect the first image to show, but not the second
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 2"]')
+  ).not.toBeInTheDocument();
+
+  // move backward in the carousel
+  const leftArrow = container.querySelector(".bi-arrow-left-circle");
+  fireEvent.click(leftArrow);
+
+  // expect the second image to show, but not the first
+  expect(
+    container.querySelector('img[alt="testing image 1"]')
+  ).not.toBeInTheDocument();
+  expect(
+    container.querySelector('img[alt="testing image 3"]')
+  ).toBeInTheDocument();
+});
+
+// As you may have noticed, the left arrow and the right arrow both do the same thing: move to the next image in the image array. Write a (failing) test that checks for this bug. In other words, write a test that expects that when you’re on the second image, clicking the left arrow will move you to the first image. Once you’ve written a failing test, fix the app so that your test turns green.
